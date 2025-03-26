547a22558271a8db17eb28ee7ebaf8408fd4d6b6
Simone <simone.brunelli3@studio.unibo.it>
2025-03-26 15:45:16 +0100

    addGradleToBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,37 @@
 plugins {
     java
     application
+    id("org.danilopianini.gradle-java-qa") version "1.77.0"
 }
 
-group = "it.unibo.exam"
-version = "1.0-SNAPSHOT"
+tasks.javadoc {
+    isFailOnError = false
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    // Aggiungi eventuali dipendenze qui
+    // Use JUnit Jupiter API for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+
+    // Use JUnit Jupiter Engine for testing.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
+
+val mainClass: String by project
 
 application {
-    mainClass.set("it.unibo.exam.Main")
+    // The following allows to run with: ./gradlew -PmainClass=it.unibo.oop.MyMainClass run
+    mainClass.set(project.properties["mainClass"].toString())
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
-
-sourceSets {
-    main {
-        java {
-            setSrcDirs(listOf("src/main/java"))
-        }
+val test by tasks.getting(Test::class) {
+    // Use junit platform for unit tests
+    useJUnitPlatform()
+    testLogging {
+        events(*(org.gradle.api.tasks.testing.logging.TestLogEvent.values())) // events("passed", "skipped", "failed")
     }
+    testLogging.showStandardStreams = true    
 }
