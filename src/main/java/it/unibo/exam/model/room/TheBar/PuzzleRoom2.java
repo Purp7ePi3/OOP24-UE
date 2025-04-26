3a64e717ce1f18d2348e9f6b68b33ca0597c480f
Daniel Alejandro <d.alejandrohorna@gmail.com>
2025-04-26 03:38:20 +0200

    Room Background Update in PuzzleRoom2

--- src/main/java/it/unibo/exam/model/room/TheBar/PuzzleRoom2.java
+++ src/main/java/it/unibo/exam/model/room/TheBar/PuzzleRoom2.java
@@ -2,7 +2,11 @@ package it.unibo.exam.model.room.TheBar;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import javax.imageio.ImageIO;
 import it.unibo.exam.controller.input.KeyHandler;
 import it.unibo.exam.model.entity.Door;
 import it.unibo.exam.model.game.GameState;
@@ -11,14 +15,26 @@ import it.unibo.exam.model.room.Room;
 import it.unibo.exam.view.panel.GamePanel;
 
 public class PuzzleRoom2 extends Room implements PuzzleRoom {
+    
     private boolean puzzleSolved = false;
     private final GameState gameState;
-    
+    private final Image backgroundImage;
+
     public PuzzleRoom2(final List<Door> doors, final GameState gameState) {
         super(Color.GRAY, doors);
         this.gameState = gameState;
+
+        Image img = null;
+        try {
+            img = ImageIO.read(new File(
+                "src/main/java/it/unibo/exam/model/room/TheBar/view/png/BarRoom.png"
+            ));
+        } catch (IOException e) {
+            System.err.println("❌ Could not load BarRoom.png: " + e.getMessage());
+        }
+        this.backgroundImage = img;
     }
-    
+
     @Override
     public void updatePuzzleLogic(final KeyHandler keyHandler) {
         int playerX = gameState.getPlayer().getX();
@@ -30,18 +46,21 @@ public class PuzzleRoom2 extends Room implements PuzzleRoom {
             if (playerX == 0 && playerY == 0 && keyHandler.interactPressed) {
                 puzzleSolved = true;
                 firstDoor.setSolved(true);
-                // Update the door in the main room
-                gameState.updateDoorState(2, true); // 2 is the index of this puzzle room
-                // Change room color
-                setBackgroundColor(Color.PINK);
+                gameState.updateDoorState(2, true); // 2 = index of this puzzle room
             }
         }
     }
     
     @Override
     public void draw(final Graphics2D g2) {
-        g2.setColor(getBackgroundColor());
-        g2.fillRect(0, 0, GamePanel.ORIGINAL_WIDTH, GamePanel.ORIGINAL_HEIGHT);
+        // Draw background image if available
+        if (backgroundImage != null) {
+            g2.drawImage(backgroundImage, 0, 0, GamePanel.ORIGINAL_WIDTH, GamePanel.ORIGINAL_HEIGHT, null);
+        } else {
+            // fallback if image failed
+            g2.setColor(getBackgroundColor());
+            g2.fillRect(0, 0, GamePanel.ORIGINAL_WIDTH, GamePanel.ORIGINAL_HEIGHT);
+        }
         
         // Draw doors
         for (Door door : getDoors()) {
@@ -49,6 +68,7 @@ public class PuzzleRoom2 extends Room implements PuzzleRoom {
             g2.fillRect(door.getX(), door.getY(), door.getWidth(), door.getHeight());
         }
         
+        // If puzzle solved
         if (puzzleSolved) {
             g2.setColor(Color.WHITE);
             g2.setFont(g2.getFont().deriveFont(24f));
